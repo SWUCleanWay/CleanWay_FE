38dff3064e704bc00d6fa3c4a2fc234d90718e40
lse415 <leesuel0379@gmail.com>
2024-04-29 00:55:41 +0900

    feat:home screen w/ mock data

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CleanWay',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF588100)),
         useMaterial3: true,
@@ -24,6 +24,23 @@ class MyApp extends StatelessWidget {
       home: const MainScreen(),
     );
   }
+}
+
+// 글 데이터 클래스
+class Post {
+  final String title;
+  final String date;
+  final String location;
+  final String issue;
+  final String imageUrl;
+
+  Post({
+    required this.title,
+    required this.date,
+    required this.location,
+    required this.issue,
+    required this.imageUrl,
+  });
 }
 
 class MainScreen extends StatefulWidget {
@@ -35,6 +52,23 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State {
   int _selectedIndex = 0;
+  bool _isCrewPostSelected = true;
+
+  // mock data list
+  final List<Map<String, dynamic>>  mockCrewData = [
+    {'title': '크루 1', 'user':'user1', 'date': '2024-XX-XX', 'capacity':'0/5'},
+    {'title': '크루 2', 'user':'user1', 'date': '2024-XX-XX', 'capacity':'3/5'},
+    {'title': '크루 3', 'user':'user1', 'date': '2024-XX-XX', 'capacity':'1/5'},
+    // ... 더 많은 데이터를 추가할 수 있습니다.
+  ];
+  final List<Map<String, dynamic>> mockReportData = [
+    {'location': '장소 1', 'date': '2024-XX-XX', 'issue': '문제점 1'},
+    {'location': '장소 2', 'date': '2024-XX-XX', 'issue': '문제점 2'},
+    {'location': '장소 3', 'date': '2024-XX-XX', 'issue': '문제점 3'},
+    // ... 더 많은 데이터를 추가할 수 있습니다.
+  ];
+
+  List<Map<String, dynamic>> get _currentList => _isCrewPostSelected ? mockCrewData : mockReportData;
 
   void _onItemSelected(int index) {
     setState(() {
@@ -73,9 +107,162 @@ class _MainScreenState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(title: Text('My first App')),
-      body: Center(
-        child: Text('Hello'),
+      appBar: AppBar(
+        title: Text(
+          'CleanWay',
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF588100),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey[300]!, width: 1)),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        _isCrewPostSelected = true;
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      color: _isCrewPostSelected ? Theme.of(context).colorScheme.primary : Colors.transparent,
+                      alignment: Alignment.center,
+                      child: Text(
+                        '크루 모집 글',
+                        style: TextStyle(
+                          color: _isCrewPostSelected ? Colors.white : Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        _isCrewPostSelected = false;
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      color: !_isCrewPostSelected ? Theme.of(context).colorScheme.primary : Colors.transparent,
+                      alignment: Alignment.center,
+                      child: Text(
+                        '제보 글',
+                        style: TextStyle(
+                          color: !_isCrewPostSelected ? Colors.white : Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              itemCount: _currentList.length,
+              itemBuilder: (context, index) {
+                var post = _currentList[index];
+                if (_isCrewPostSelected) {
+                  // 크루 모집 글 카드
+                  return GestureDetector(
+                    onTap: () {
+                      // TODO: 여기에 상세 페이지로의 경로를 설정하면 됩니다.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('상세 페이지로 이동: ${post['title']}'),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(post['title'], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                            SizedBox(height: 8),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text('작성자: ${post['user']}', style: TextStyle(fontSize: 16)),
+                                Text(' | ', style: TextStyle(fontSize: 16)),
+                                Text('작성 시간: ${post['date']}', style: TextStyle(fontSize: 16)),
+                              ],
+                            ),
+
+                            SizedBox(height: 8),
+                            Text('모집 인원: ${post['capacity']}', style: TextStyle(fontSize: 16)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    child: AspectRatio(
+                      aspectRatio: 4 / 3,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200], // 임시 배경색
+                        ),
+                        child: Stack(
+                          children: [
+                            // 포스트 내용
+                            Positioned(
+                              left: 16,
+                              top: 16,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(post['date'],
+                                      style: TextStyle(fontSize: 16)),
+                                  SizedBox(height: 8),
+                                  Text(post['location'],
+                                      style: TextStyle(fontSize: 16)),
+                                  SizedBox(height: 8),
+                                  Text(post['issue'],
+                                      style: TextStyle(fontSize: 16)),
+                                ],
+                              ),
+                            ),
+                            // 즐겨찾기 버튼
+                            Positioned(
+                              right: 16,
+                              bottom: 16,
+                              child: IconButton(
+                                icon: const Icon(Icons.favorite_border),
+                                onPressed: () {
+                                  // 즐겨찾기 기능 구현
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                };
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: Builder(
         builder: (BuildContext context) {
