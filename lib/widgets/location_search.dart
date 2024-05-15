545fbaeb9ce1fffad1de0b9728e3b5bdaaff4057
lse415 <leesuel0379@gmail.com>
2024-05-15 21:40:57 +0900

    feat:장소검색-마커

--- lib/widgets/location_search.dart
+++ lib/widgets/location_search.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LocationSearch extends StatefulWidget {
   @override
@@ -14,7 +17,12 @@ class _LocationSearchState extends State<LocationSearch> {
   @override
   void initState() {
     super.initState();
+    loadEnv();
     requestLocationPermission();
+  }
+
+  Future<void> loadEnv() async {
+    await dotenv.load();
   }
 
   Future<void> requestLocationPermission() async {
@@ -27,22 +35,23 @@ class _LocationSearchState extends State<LocationSearch> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar
-        (title: Text('위치 검색'),
+      appBar: AppBar(
+        title: Text('위치 검색'),
         actions: [
           IconButton(
             icon: Icon(Icons.search),
             onPressed: () => searchLocation(_searchController.text),
           ),
-        ],),
+        ],
+      ),
       body: Column(
-          children: [
-            Padding(
+        children: [
+          Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               controller: _searchController,
               decoration: InputDecoration(
-                hintText: '검색할 위치 입력',
+                hintText: '검색할 장소 입력',
                 suffixIcon: IconButton(
                   icon: Icon(Icons.clear),
                   onPressed: () => _searchController.clear(),
@@ -51,8 +60,8 @@ class _LocationSearchState extends State<LocationSearch> {
               onSubmitted: (value) => searchLocation(value),
             ),
           ),
-            Expanded(
-              child: NaverMap(
+          Expanded(
+            child: NaverMap(
               onMapReady: (controller) {
                 _controller = controller;
                 print("네이버 맵 로딩됨!");
@@ -60,18 +69,46 @@ class _LocationSearchState extends State<LocationSearch> {
               onMapTapped: (point, latLng) {
                 print("맵 탭 위치: $latLng");
               },
-              /*locationButtonEnable: true,
-              initLocationTrackingMode: LocationTrackingMode.Follow,*/
-              ),
             ),
-          ],
-        ),
-      );
-    }
+          ),
+        ],
+      ),
+    );
   }
 
-  void searchLocation(String query) {
-    // 네이버 검색 API 통합 또는 사용자 정의 검색 로직 구현
-    print("검색 진행: $query");
-    // 여기에 API 호출 코드 및 결과 처리 로직을 추가하십시오.
+  void searchLocation(String query) async {
+    var clientId = dotenv.env['X_NAVER_CLIENT_ID']!;
+    var clientSecret = dotenv.env['X_NAVER_CLIENT_SECRET']!;
+    String encodedQuery = Uri.encodeComponent(query);
+    var url = Uri.parse(
+        'https://openapi.naver.com/v1/search/local.json?query=$encodedQuery');
+    var response = await http.get(url, headers: {
+      'X-Naver-Client-Id': clientId,
+      'X-Naver-Client-Secret': clientSecret,
+    });
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      var items = data['items'];
+      if (items.isNotEmpty) {
+        var item = items[0];
+        // 정수형 좌표를 실제 위도와 경도로 변환
+        double lat = double.parse(item['mapy']) * 1e-7;
+        double lng = double.parse(item['mapx']) * 1e-7;
+        print("위도, 경도 : $lat $lng"); // 수정된 위도와 경도 출력
+        setState(() {
+          _controller.updateCamera(
+              NCameraUpdate.scrollAndZoomTo(target: NLatLng(lat, lng))
+          );
+          var marker = NMarker(
+            id: "resultMarker",
+            position: NLatLng(lat, lng),
+          );
+          _controller.addOverlay(marker);
+        });
+      }
+    } else {
+      print('Failed to fetch data with status code: ${response.statusCode}');
+    }
   }
+}
