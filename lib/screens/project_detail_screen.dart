52bf22c06a9c9eba34912f2d7bee5398a4a16dc4
lse415 <leesuel0379@gmail.com>
2024-05-18 18:24:28 +0900

    크루모집 상세, 내크루목록 api

--- lib/screens/project_detail_screen.dart
+++ lib/screens/project_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-// 모델 클래스 정의
 class CrewDetail {
   final String title;
   final String author;
@@ -25,27 +27,84 @@ class CrewDetail {
     required this.capacity,
     required this.additionalInfo,
   });
+
+  factory CrewDetail.fromJson(Map<String, dynamic> json) {
+    return CrewDetail(
+      title: json['crewName'],
+      author: json['userNickname'],
+      createdAt: json['crewWriteTime'],
+      location: json['projectSName'],
+      route: json['projectDName'],
+      date: json['projectDate'],
+      time: json['projectTime'],
+      participants: json['memberCount'],
+      capacity: json['crewRecruitment'],
+      additionalInfo: json['crewContent'],
+    );
+  }
 }
 
-class ProjectDetailScreen extends StatelessWidget {
-  final CrewDetail crewDetail = CrewDetail(
-    title: '크루AAA',
-    author: 'user1',
-    createdAt: '2024년 04월 22일',
-    location: '서울시 노원구 공릉동 서울산업대학교',
-    route: '출발지',
-    date: '2024년 04월 22일',
-    time: '10:00',
-    participants: 0,
-    capacity: 5,
-    additionalInfo: '같이 플로깅 하실 분들 모집합니다~',
-  );
+class ProjectDetailScreen extends StatefulWidget {
+  final int crewNumber;
+
+  ProjectDetailScreen({required this.crewNumber});
+
+  @override
+  _ProjectDetailScreenState createState() => _ProjectDetailScreenState();
+}
+
+class _ProjectDetailScreenState extends State<ProjectDetailScreen> {
+  late Future<CrewDetail> crewDetailFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    crewDetailFuture = fetchCrewDetail();
+  }
+
+  Future<CrewDetail> fetchCrewDetail() async {
+    // Mock data를 사용하여 CrewDetail 객체 생성
+    var mockData = {
+      "crewNumber": 1,
+      "userNumber": 1,
+      "crewName": "크루AAA",
+      "crewWriteTime": "2024년 04월 22일",
+      "crewContent": "같이 플로깅 하실 분들 모집합니다~",
+      "crewRecruitment": 5,
+      "crewRoleNumber": 1,
+      "crewProjectNumber": 1,
+      "projectDate": "2024년 04월 22일",
+      "projectTime": "10:00",
+      "projectSLng": 127.058,
+      "projectSLat": 37.512,
+      "projectSName": "서울시 노원구 공릉동 서울산업대학교",
+      "projectVLng": 127.058,
+      "projectVLat": 37.512,
+      "projectVName": "출발지",
+      "projectDLng": 127.061,
+      "projectDLat": 37.510,
+      "projectDName": "목적지",
+      "memberCount": 0,
+      "userNickname": "user1"
+    };
+
+    // 실제 서버를 사용할 때
+    // String url = '${dotenv.env['NGROK_URL']}/crew/detail/${widget.crewNumber}';
+    // var response = await http.get(Uri.parse(url));
+    // if (response.statusCode == 200) {
+    //   return CrewDetail.fromJson(json.decode(response.body));
+    // } else {
+    //   throw Exception('Failed to load crew details');
+    // }
+
+    return CrewDetail.fromJson(mockData);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: Text('Project Details'),
         actions: [
           Icon(Icons.share),
           SizedBox(width: 16),
@@ -53,127 +112,71 @@ class ProjectDetailScreen extends StatelessWidget {
           SizedBox(width: 16),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(20.0, 0, 0, 0),
-              child: Text(
-                crewDetail.title,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.fromLTRB(20.0, 0, 0, 0),
-              //leading: Icon(Icons.account_circle),
-              title: Text(crewDetail.author),
-              subtitle: Text(crewDetail.createdAt),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '모임 위치',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text('${crewDetail.location}'),
-                  Divider(),
-                  Text(
-                      '루트',
-                    style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  ),
-                  SizedBox(height: 10),
-                  Text('${crewDetail.route}'),
-                  Divider(),
-                  Text(
-                      '날짜',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text('${crewDetail.date}'),
-                  Divider(),
-                  Text(
-                      '시간',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text('${crewDetail.time}'),
-                  Divider(),
-                  Text(
-                      '참여 인원',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text('${crewDetail.participants} / ${crewDetail.capacity}명'),
-                  Divider(),
-                  Text(
-                      '추가 정보',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text('${crewDetail.additionalInfo}'),
-                ],
-              ),
-            ),
-          ],
-        ),
+      body: FutureBuilder<CrewDetail>(
+        future: crewDetailFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              return buildDetailLayout(snapshot.data!);
+            } else if (snapshot.hasError) {
+              return Text("Error: ${snapshot.error}");
+            }
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // 둘러보기 로직
-                  },
-                  child: Text('둘러보기'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Theme.of(context).colorScheme.primary,
-                  ),
-                ),
-              ),
-              SizedBox(width: 10),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // 참여하기 로직
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    foregroundColor: Colors.white,
-                  ),
-                  child: Text('참여하기'),
-                ),
-              ),
-            ],
+    );
+  }
+
+  Widget buildDetailLayout(CrewDetail detail) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(20.0, 20.0, 0, 0),
+            child: Text(
+              detail.title,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
+          ListTile(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
+            title: Text(detail.author),
+            subtitle: Text(detail.createdAt),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('모임 위치', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text(detail.location),
+                Divider(),
+                Text('루트', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text(detail.route),
+                Divider(),
+                Text('날짜', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text(detail.date),
+                Divider(),
+                Text('시간', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text(detail.time),
+                Divider(),
+                Text('참여 인원', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text('${detail.participants} / ${detail.capacity}명'),
+                Divider(),
+                Text('추가 정보', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text(detail.additionalInfo),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
